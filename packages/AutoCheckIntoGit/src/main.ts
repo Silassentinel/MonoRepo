177f2882b0992d2f5df44b24a4491f7513fe6605
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2022-12-27 11:51:56 +0100

    auto commit 1.672.138.316.076

--- packages/AutoCheckIntoGit/src/main.ts
+++ packages/AutoCheckIntoGit/src/main.ts
@@ -4,6 +4,7 @@
 
 // #region imports
 import dotenv from 'dotenv';
+import ToolBox from '@silassentinel/toolbox/src/Class/ToolBox';
 // #endregion
 
 // #region config
@@ -18,17 +19,16 @@ const main = async () => {
   const rootDir = process.env.PWD;
   console.log(`Root directory: ${rootDir}`);
   // Check via helper which os is running
-  const helper = await import('./Class/Helper').then((m) => m.default);
 
   console.log('*********************************');
-  console.log(`OS: ${await helper.GetOS()}`);
+  console.log(`OS: ${await ToolBox.GetOS()}`);
   console.log('*********************************');
 
-  if (await helper.IsGitInstalled()) {
+  if (await ToolBox.IsGitInstalled()) {
     // if os is windows run script/win.ps1 or script/win.bat if no powershell is present
-    if (await helper.GetOS() === 'windows') {
+    if (await ToolBox.GetOS() === 'windows') {
       try {
-        const result = await helper.Execute('powershell.exe -version');
+        const result = await ToolBox.Execute('powershell.exe -version');
         console.error(result);
       } catch (error) {
         console.error(error);
@@ -36,14 +36,14 @@ const main = async () => {
     }
 
     // if os is linux run script/linux.sh
-    if (await helper.GetOS() === 'linux') {
+    if (await ToolBox.GetOS() === 'linux') {
       try {
         // const curDirr = await helper.execute('pwd');
         // console.log(`current directory is: ${curDirr}`);
         const projectRoot = process.env.ROOTDIR;
-        const result = await helper.Execute(`cd ${projectRoot} && git status`) as string;
+        const result = await ToolBox.Execute(`cd ${projectRoot} && git status`) as string;
         if (result.includes('modified')) {
-          await helper.Execute(`cd ${projectRoot} && git add . && git commit -m "auto commit ${Date.now().toLocaleString('nl-Be')}" && git push`);
+          await ToolBox.Execute(`cd ${projectRoot} && git add . && git commit -m "auto commit ${Date.now().toLocaleString('nl-Be')}" && git push`);
         }
         console.log(result);
       } catch (error) {
@@ -51,9 +51,9 @@ const main = async () => {
       }
     }
     // if os is mac run script/mac.sh
-    if (await helper.GetOS() === 'mac') {
+    if (await ToolBox.GetOS() === 'mac') {
       try {
-        const result = await helper.Execute('bash -version');
+        const result = await ToolBox.Execute('bash -version');
         console.error(result);
       } catch (error) {
         // eslint-disable-next-line no-console
--- packages/ToolBox/src/Class/ToolBox.ts
+++ packages/ToolBox/src/Class/ToolBox.ts
@@ -2,12 +2,12 @@
 import Client from '@silassentinel/frontendlib/src/types/Client';
 import Job from '@silassentinel/frontendlib/src/types/Job';
 import Blog from '@silassentinel/frontendlib/src/types/Blog';
+// #endregion
 // #region imports
 import { Hash } from 'crypto';
 import { v4 as UUIDV4, V4Options } from 'uuid';
+import { ExecException, exec } from 'node:child_process';
 import ToolBoxError from '../utilities/Errors/ToolBoxError';
-// #endregion
-
 // #endregion
 
 class ToolBox {
@@ -63,6 +63,72 @@ class ToolBox {
    * @returns the current year
    */
   static GetCurrentYear = () => new Date().getFullYear();
+
+  /**
+   * This function will check if git is installed on the system, if it's installed it will return the version of git.
+   * @return {Promise<boolean>} A promise that resolve to true if git is installed, or false if not
+   */
+  static IsGitInstalled = async () => {
+    const git = await ToolBox.Execute('git --version');
+    if (!git) {
+      return false;
+    }
+    return true;
+  };
+
+  /**
+   * This function will check which os is running.
+   * @returns {Promise<string>} A promise that resolve to the os type
+   */
+  static GetOS = async (): Promise<string> => {
+    const os = await import('os');
+    const osFlavor = os.type().toLowerCase();
+    if (osFlavor === 'linux') return 'linux';
+    if (osFlavor === 'darwin') return 'mac';
+    if (osFlavor === 'windows_NT') return 'windows';
+    return osFlavor;
+  };
+
+  /**
+ * @param {string} command A shell command to execute
+ * @return {Promise<string>} A promise that resolve to the output of the shell command, or an error
+ * @example const output = await execute("ls -alh");
+ */
+  static Execute = (command: string) =>
+    /**
+     * @param {Function} resolve A function to call when the shell command is executed successfully
+     * @param {Function} reject A function to call when the shell command is executed with an error
+     * @return {Promise<string>} A promise that resolve to the output of the shell command, or an error
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+     */
+    // eslint-disable-next-line implicit-arrow-linebreak
+    new Promise((resolve, reject) => {
+    /**
+     * @param {Error} error An error triggered during the execution of the childProcess.exec command
+     * @param {string|Buffer} standardOutput The result of the shell command execution
+     * @param {string|Buffer} standardError The error resulting of the shell command execution
+     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+     */
+      exec(command, (error : ExecException | null, standardOutput : string, standardError: string) => {
+        if (error) {
+          reject();
+          return;
+        }
+        if (standardError) {
+          reject(standardError);
+          return;
+        }
+        resolve(standardOutput);
+      });
+    });
+
+  /**
+   * This function will run every 5 minutes. It takes a function as parameter and will execute it every 5 minutes.
+   * @param {Function} func The function to execute every 5 minutes.
+   */
+  static runEveryFiveMinutes = (func: (p1?: string, p2?: string) => any) => {
+    setInterval(() => func, 300000);
+  };
   // #endregion
 
   // #region react helper methods
