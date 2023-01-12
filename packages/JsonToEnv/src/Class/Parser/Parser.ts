cc51cc449776d685ccb3155eea9c8426bce1a956
silassentinel <53788992+Silassentinel@users.noreply.github.com>
2023-01-12 15:28:18 +0100

    parser mini work

--- packages/JsonToEnv/src/Class/Parser/Parser.ts
+++ packages/JsonToEnv/src/Class/Parser/Parser.ts
@@ -1,8 +1,10 @@
 // #region imports
 import Toolbox from '@silassentinel/toolbox/src/Class/ToolBox';
-import ToolBoxError from '@silassentinel/toolbox/src/utilities/Errors/ToolBoxError';
+// import ToolBoxError from '@silassentinel/toolbox/src/utilities/Errors/ToolBoxError';
+import dotenv from 'dotenv';
 // #endregion
 // #region config
+dotenv.config();
 // #endregion
 
 /**
@@ -12,6 +14,11 @@ import ToolBoxError from '@silassentinel/toolbox/src/utilities/Errors/ToolBoxErr
 class Parser {
   // #region props
   /**
+   * Jobject after parsing
+   */
+  jobject!: [key: string, value: string];
+
+  /**
      * Path to the json file.
      * @type {string}
      */
@@ -19,48 +26,41 @@ class Parser {
 
   // #endregion
   // #region ctor
-  constructor(pathTojsonFile: string) {
-    const sanitizedPath = Toolbox.SanitizeFilePath(pathTojsonFile);
+  constructor(pathToJsonFile: string) {
+    const sanitizedPath = Toolbox.SanitizeFilePath(pathToJsonFile);
     if (Toolbox.IsString(sanitizedPath)) this.pathToJsonFile = sanitizedPath as string;
   }
+
   // #endregion
   // #region methods
   // #region getters/setters
   // #endregion
   // #region public methods
-  // #endregion
-  // #region private helpers
-
-  /**
-   * This method will be responsible for parsing the json file and converting it
-   * at run time to sytem environment variables.
-   * @param {string} pathToJsonFile Path to the json file.
-   * @returns {void}
-   */
-  private parseJsonFile = async () => {
+  public parseJsonFile = async () => {
     // #region sanitize
-    if (!Toolbox.IsStringEmpty(this.pathToJsonFile)) throw new Error('Invalid path to json file.');
-    const toolboxError = Toolbox.SanitizeFilePath(this.pathToJsonFile);
-    if (toolboxError instanceof ToolBoxError) throw toolboxError;
+    if (!this.pathToJsonFile) throw new Error('Invalid path to json file.');
+    this.pathToJsonFile = Toolbox.SanitizeFilePath(this.pathToJsonFile).toString();
     // #endregion
 
     // #region read json file
     const jsonFile = await Toolbox.ReadJsonFile(this.pathToJsonFile);
-    if (jsonFile instanceof ToolBoxError) throw jsonFile;
     // #endregion
 
     // #region parse json file
-    const json = JSON.parse(jsonFile as string);
+    this.jobject = JSON.parse(jsonFile.toString());
     // #endregion
 
     // #region set environment variables
-    this.SetEnvironmentVariables(json);
+    this.SetEnvironmentVariables();
     // #endregion
   };
 
-  private SetEnvironmentVariables = (json: any) => {
-    Object.keys(json).forEach((key) => {
-      process.env[key] = json[key];
+  /**
+   * This method will set the environment variables.
+   */
+  private SetEnvironmentVariables = () => {
+    Object.keys(this.jobject).forEach((key: string) => {
+      process.env[key] = this.jobject[key];
     });
   };
   // #endregion
